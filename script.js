a00510b57345b805daec8965049c0098b238b71b
Debajit Paul <debajitfan.of@gmail.com>
2023-02-23 00:46:06 +0530

    update: style and added search

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // API key : 597932d351b44a3b95ee56a78489f7a6
 
-// let countryName = document.getElementById("country-name"); 
-// console.log(countryName.options[countryName.selectedIndex].text);
-let headlineList = document.getElementById("headline-list");
+const headlineList = document.getElementById("headline-list");
+const inputSearch = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const searchNewslist = document.getElementById("searchNews-list");
+let searchHeader = document.getElementById("search-Header");
 
 window.onload = () => {
     getTopHeadline();
   }
 
-  const fetchNews = (url) => {
+  const fetchNews = (url,showNews) => {
     fetch(url)
     .then(response => {
         if(response.ok){
@@ -23,28 +25,25 @@ window.onload = () => {
     })
   }
 
+  const searchNews =data =>{
+    
+    searchHeader.innerText = `Search Result: ${inputSearch.value}`;
 
-  const showNews = (data) =>{
-
+    while(searchNewslist.hasChildNodes()){
+        searchNewslist.removeChild(searchNewslist.firstChild);
+    }
     for(let news of data){
         // create <li></li>
         let newsList = document.createElement("li");
         newsList.classList.add("news-item");
-        headlineList.appendChild(newsList);
+        searchNewslist.appendChild(newsList);
         // create <div></div>
         let newDiv = document.createElement("div");
         newDiv.className = "news"
-        newDiv.style.background = `rgba(0,0,0,0.5)url(${news.urlToImage})`;
+        newDiv.style.background = `url("${news.urlToImage}")rgba(0,0,0,0.7)`;
         newDiv.style.backgroundSize = "cover";
-        newDiv.style.backgroundRepeat = "no-repeat";
         newsList.appendChild(newDiv);
-        //create <img>
-        // let newsBanner = document.createElement("img");
-        // newsBanner.className = "news-banner";
-        // newsBanner.src = `${news.urlToImage}`;
-        // newDiv.appendChild(newsBanner);
-        // create news heading
-        let newsHeading = document.createElement("h3");
+        let newsHeading = document.createElement("h4");
         newsHeading.className = "news-heading";
         newsHeading.innerText = `${news.title}`;
         newDiv.appendChild(newsHeading);
@@ -57,12 +56,54 @@ window.onload = () => {
         let readBtn = document.createElement("a");
         readBtn.className = "read-button"
         readBtn.href = `${news.url}`;
-        readBtn.innerHTML = "Learn more";
+        readBtn.innerHTML = 'Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i>';
         newDiv.appendChild(readBtn);
     }
   }
 
-  const getTopHeadline = () =>{
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=597932d351b44a3b95ee56a78489f7a6";
-    fetchNews(url);
+  const TopHeadline = (data) =>{
+    
+    for(let news of data){
+        // create <li></li>
+        let newsList = document.createElement("li");
+        newsList.classList.add("news-item");
+        headlineList.appendChild(newsList);
+        // create <div></div>
+        let newDiv = document.createElement("div");
+        newDiv.className = "news"
+        newDiv.style.background = `url("${news.urlToImage}")rgba(0,0,0,0.7)`;
+        newDiv.style.backgroundSize = "cover";
+        newsList.appendChild(newDiv);
+        let newsHeading = document.createElement("h4");
+        newsHeading.className = "news-heading";
+        newsHeading.innerText = `${news.title}`;
+        newDiv.appendChild(newsHeading);
+        // create news description
+        // let newsDescription = document.createElement("p");
+        // newsDescription.className = "news-description"
+        // newsDescription.innerText = `${news.description}`;
+        // newDiv.appendChild(newsDescription);
+        // create read more button
+        let readBtn = document.createElement("a");
+        readBtn.className = "read-button"
+        readBtn.href = `${news.url}`;
+        readBtn.innerHTML = 'Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i>';
+        newDiv.appendChild(readBtn);
+    }
+
+    
   }
+
+  
+
+  const getTopHeadline = () =>{
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=597932d351b44a3b95ee56a78489f7a6";
+    fetchNews(url,TopHeadline);
+  }
+
+  searchButton.addEventListener ("click",e => {
+    e.preventDefault();
+    let search = inputSearch.value;
+    let url = `https://newsapi.org/v2/top-headlines?q=${search}&apiKey=597932d351b44a3b95ee56a78489f7a6`
+    fetchNews(url,searchNews);
+  })
