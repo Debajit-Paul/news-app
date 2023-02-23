87c3e2dce3c4d11077f814545270caddfee642c8
Debajit Paul <debajitfan.of@gmail.com>
2023-02-24 03:41:15 +0530

    initial carousel fix

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // API key : 597932d351b44a3b95ee56a78489f7a6
 
+const headlinecontainer = document.getElementById("headline-container");
 const headlineList = document.getElementById("headline-list");
-const inputSearch = document.getElementById("search-input");
-const searchButton = document.getElementById("search-button");
-const searchNewslist = document.getElementById("searchNews-list");
-let searchHeader = document.getElementById("search-Header");
 
 window.onload = () => {
     getTopHeadline();
   }
+
 
   const fetchNews = (url,showNews) => {
     fetch(url)
@@ -25,49 +23,15 @@ window.onload = () => {
     })
   }
 
-  const searchNews =data =>{
-    
-    searchHeader.innerText = `Search Result: ${inputSearch.value}`;
-
-    while(searchNewslist.hasChildNodes()){
-        searchNewslist.removeChild(searchNewslist.firstChild);
-    }
-    for(let news of data){
-        // create <li></li>
-        let newsList = document.createElement("li");
-        newsList.classList.add("news-item");
-        searchNewslist.appendChild(newsList);
-        // create <div></div>
-        let newDiv = document.createElement("div");
-        newDiv.className = "news"
-        newDiv.style.background = `url("${news.urlToImage}")rgba(0,0,0,0.7)`;
-        newDiv.style.backgroundSize = "cover";
-        newsList.appendChild(newDiv);
-        let newsHeading = document.createElement("h4");
-        newsHeading.className = "news-heading";
-        newsHeading.innerText = `${news.title}`;
-        newDiv.appendChild(newsHeading);
-        // create news description
-        // let newsDescription = document.createElement("p");
-        // newsDescription.className = "news-description"
-        // newsDescription.innerText = `${news.description}`;
-        // newDiv.appendChild(newsDescription);
-        // create read more button
-        let readBtn = document.createElement("a");
-        readBtn.className = "read-button"
-        readBtn.href = `${news.url}`;
-        readBtn.innerHTML = 'Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i>';
-        newDiv.appendChild(readBtn);
-    }
-  }
 
   const TopHeadline = (data) =>{
-    
+
     for(let news of data){
         // create <li></li>
         let newsList = document.createElement("li");
         newsList.classList.add("news-item");
         headlineList.appendChild(newsList);
+
         // create <div></div>
         let newDiv = document.createElement("div");
         newDiv.className = "news"
@@ -78,11 +42,7 @@ window.onload = () => {
         newsHeading.className = "news-heading";
         newsHeading.innerText = `${news.title}`;
         newDiv.appendChild(newsHeading);
-        // create news description
-        // let newsDescription = document.createElement("p");
-        // newsDescription.className = "news-description"
-        // newsDescription.innerText = `${news.description}`;
-        // newDiv.appendChild(newsDescription);
+        
         // create read more button
         let readBtn = document.createElement("a");
         readBtn.className = "read-button"
@@ -90,8 +50,6 @@ window.onload = () => {
         readBtn.innerHTML = 'Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i>';
         newDiv.appendChild(readBtn);
     }
-
-    
   }
 
   
@@ -101,9 +59,31 @@ window.onload = () => {
     fetchNews(url,TopHeadline);
   }
 
-  searchButton.addEventListener ("click",e => {
-    e.preventDefault();
-    let search = inputSearch.value;
-    let url = `https://newsapi.org/v2/top-headlines?q=${search}&apiKey=597932d351b44a3b95ee56a78489f7a6`
-    fetchNews(url,searchNews);
+  document.addEventListener("click", e =>{
+    let handle;
+    if(e.target.matches(".handle")){
+      handle = e.target;
+    }else{
+      handle = e.target.closest(".handle")
+    }
+    if(handle != null){
+      onHandleClick(handle);
+    }
   })
+
+  function onHandleClick(handle) {
+    // const slider = handle.closest(".container").querySelector("#headline-container").querySelector("headline-list");
+    const slider = document.querySelector("#headline-list");
+    // console.log(document.querySelector("#headline-list"));
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"));
+    if(handle.classList.contains("left-handle")){
+      slider.style.setProperty("--slider-index", sliderIndex - 1);
+      console.log(getComputedStyle(slider).getPropertyValue("--slider-index"));
+      // slider.style.transform = `translateX(cal(var(${sliderIndex - 1}) * -100%))`;
+      // console.log(slider.style.transform = `translateX(cal(var(${sliderIndex - 1}) * -100%))`);
+    }
+    if(handle.classList.contains("right-handle")){
+      // slider.style.transform = `translateX(cal(var(${sliderIndex + 1}) * -100%))`;
+      slider.style.setProperty("--slider-index", sliderIndex + 1);
+    }
+  }
